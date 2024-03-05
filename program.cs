3b01782a540abca72115b7c39c2375723edd3c88
Bogdan Kozak <bkozak04@gmail.com>
2024-03-05 20:15:23 +0200

    Added receiving the .md file by the program

--- program.cs
+++ program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
+    static string text =@"";
+    static string path = "", name = "";
+    
     static void Main()
     {
+        try
+        {
+            AskForFile(ref text, ref path, ref name);
+        }
+        catch(Exeption ex)
+        {
+            Console.Error.WriteLine("Error: " + ex);
+            Console.ReadKey();
+        }
+    }
 
+    static void AskForFile(ref string text, ref string path, ref string name)
+    {
+        Console.WriteLine("Enter the path to the text file:\nExample: \"C:\\Documents\\Super Secret\\file.md\"");
+        string filePath = Console.ReadLine();
+        if (Path.GetExtension(filePath) == ".md")
+        {
+            if (File.Exists(filePath))
+            {
+                text = File.ReadAllText(filePath);
+                path = Path.GetDirectoryName(filePath) + "\\";
+                name = Path.GetFileNameWithoutExtension(filePath);
+            }
+            else
+            {
+                Console.WriteLine("File does not exist. Please try again.");
+                AskForFile(ref text, ref path, ref name);
+            }
+        }
+        else
+        {
+            Console.WriteLine("The extention must be .md. Please try again.");
+            AskForFile(ref text, ref path, ref name);
+        }
     }
 }
