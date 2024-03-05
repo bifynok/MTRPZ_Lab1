4337dd28b168a7af469c57fa0e3ecbfe307fa172
Bogdan Kozak <bkozak04@gmail.com>
2024-03-05 20:38:46 +0200

    Added italic punctuation check

--- program.cs
+++ program.cs
@@ -10,6 +10,7 @@ class Program
         try
         {
             AskForFile(ref text, ref path, ref name);
+            ChangeText();
         }
         catch(Exeption ex)
         {
@@ -42,4 +43,72 @@ class Program
             AskForFile(ref text, ref path, ref name);
         }
     }
+
+    static void ChangeText()
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            CheckItalic(ref text, i);
+        }
+    }
+
+    static void CheckItalic(ref string text, int i)
+    {
+        if (text[i] == '_')
+        {
+            if (i == 0 || (i > 0 && !char.IsLetterOrDigit(text[i - 1])))
+            {
+                if (i < text.Length - 1)
+                {
+                    if (char.IsLetterOrDigit(text[i + 1]))
+                    {
+                        text = text.Remove(i, 1);
+                        text = text.Insert(i, "<i>");
+                        CloseItalic(ref text, i);
+                    }
+                    else if (char.IsPunctuation(text[i + 1]) || text[i + 1] == '`')
+                    {
+                        text = text.Remove(i, 1);
+                        text = text.Insert(i, "<i>");
+                        CloseItalic(ref text, i);
+                    }
+                }
+            }
+        }
+    }
+
+    static void CloseItalic(ref string text, int i)
+    {
+        for (int j = i + 1; j < text.Length; j++)
+        {
+            if (text[j] == '_')
+            {
+                if (j == text.Length-1 || (j < text.Length-1 && !char.IsLetterOrDigit(text[j+1])))
+                {
+                    if (j > 0)
+                    {
+                        if (char.IsLetterOrDigit(text[j - 1]))
+                        {
+                            text = text.Remove(j, 1);
+                            text = text.Insert(j, "</i>");
+                            break;
+                        }
+                        else if (char.IsPunctuation(text[j - 1]) || text[j - 1] == '`' || text[j - 1] == '>')
+                        {
+                            text = text.Remove(j, 1);
+                            text = text.Insert(j, "</i>");
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (j == text.Length - 1)
+            {
+                throw new Exception("Unclosed Punctuation");
+            }
+        }
+    }
+
+
+
 }
